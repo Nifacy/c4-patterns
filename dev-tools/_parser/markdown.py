cc3fbeec06cefe7534c6710398ebc37090bed5c0
Nifacy <alexgrishin5556@gmail.com>
2025-07-10 00:01:00 +0300

    change validation on parser

---
+++ dev-tools/_parser/markdown.py
@@ -0,0 +1,194 @@
+from __future__ import annotations
+
+from typing import Callable, NoReturn, Sequence, override
+import marko.element
+import marko.block
+import marko.inline
+import re
+
+from . import base
+
+
+class MarkdownCursor(base.Cursor[marko.element.Element]):
+    def __init__(self, elements: Sequence[marko.element.Element], index: int = 0):
+        self.__elements = elements
+        self.__index = index
+
+    def next(self) -> tuple[marko.element.Element | None, MarkdownCursor]:
+        if self.__index >= len(self.__elements):
+            return None, self
+        return self.__elements[self.__index], MarkdownCursor(
+            self.__elements, self.__index + 1
+        )
+
+
+class _ElementParser[_V, _T: marko.element.Element](
+    base.IParser[_V, marko.element.Element]
+):
+    def __init__(
+        self,
+        expected_type: type[_T],
+        children_parser: base.IParser[_V, marko.element.Element] | None = None,
+        parse_element: Callable[[_V, _T], _V] | None = None,
+    ):
+        self.__expected_type = expected_type
+        self.__children_parser = children_parser
+        self.__parse_element = parse_element
+
+    def parse(
+        self, state: base.State[_V, marko.element.Element]
+    ) -> base.State[_V, marko.element.Element]:
+        parsed_value, cursor = state
+        current_element, next_cursor = cursor.next()
+
+        if current_element is None:
+            self.__raise_eof_error()
+
+        if not isinstance(current_element, self.__expected_type):
+            self.__raise_wrong_type_error(current_element)
+
+        self._validate_element(current_element)
+
+        if self.__parse_element is not None:
+            parsed_value = self.__parse_element(parsed_value, current_element)
+
+        if self.__children_parser is not None:
+            parsed_value, _ = self.__children_parser.parse(
+                (
+                    parsed_value,
+                    MarkdownCursor(current_element.children),  # type: ignore
+                )
+            )
+
+        return parsed_value, next_cursor
+
+    def __raise_eof_error(self) -> NoReturn:
+        raise base.ParseError(
+            f"Expected {self.__expected_type.__name__}, but found EOF"
+        )
+
+    def __raise_wrong_type_error(self, element: marko.element.Element) -> NoReturn:
+        raise base.ParseError(
+            f"Expected {self.__expected_type.__name__}, but found {type(element).__name__}"
+        )
+
+    def _validate_element(self, element: _T) -> None:
+        """
+        Override this method to implement specific element validation logic.
+
+        :param element: The element to validate.
+        """
+        pass
+
+
+class EOF[_V](base.IParser[_V, marko.element.Element]):
+    def parse(
+        self, state: base.State[_V, marko.element.Element]
+    ) -> base.State[_V, marko.element.Element]:
+        parsed_value, cursor = state
+        current_element, next_cursor = cursor.next()
+
+        if current_element is not None:
+            raise base.ParseError(f"Expected EOF, but found {current_element}")
+
+        return parsed_value, next_cursor
+
+
+class RawText[_V](_ElementParser[_V, marko.inline.RawText]):
+    def __init__(
+        self,
+        regex: re.Pattern[str],
+        parse_element: Callable[[_V, marko.inline.RawText], _V] | None = None,
+    ):
+        super().__init__(
+            expected_type=marko.inline.RawText,
+            parse_element=parse_element,
+        )
+        self.__regex = regex
+
+    @override
+    def _validate_element(self, element: marko.inline.RawText) -> None:
+        if self.__regex.fullmatch(element.children) is None:
+            raise base.ParseError(
+                f"Raw text does not match regex: {self.__regex.pattern}"
+            )
+
+
+class BlankLines[_V](base.IParser[_V, marko.element.Element]):
+    def parse(
+        self, state: base.State[_V, marko.element.Element]
+    ) -> base.State[_V, marko.element.Element]:
+        parsed_data, cursor = state
+
+        while True:
+            current_element, next_cursor = cursor.next()
+
+            if current_element is None:
+                break
+
+            if not isinstance(current_element, marko.block.BlankLine):
+                break
+
+            cursor = next_cursor
+
+        return parsed_data, cursor
+
+
+# TODO: rewrite parse children element on client side
+class Header[_V](_ElementParser[_V, marko.block.Heading]):
+    def __init__(
+        self,
+        level: int,
+        text_expr: re.Pattern[str],
+        parse_element: Callable[[_V, marko.block.Heading], _V] | None = None,
+    ):
+        super().__init__(
+            expected_type=marko.block.Heading,
+            children_parser=base.Chain(RawText(text_expr), EOF()),
+            parse_element=parse_element,
+        )
+        self.__level = level
+
+    @override
+    def _validate_element(self, element: marko.block.Heading) -> None:
+        if element.level != self.__level:
+            raise base.ParseError(
+                f"Expected heading with level {self.__level} but found {element.level}"
+            )
+
+
+class List[_V](_ElementParser[_V, marko.block.List]):
+    def __init__(
+        self, children_parser: base.IParser[_V, marko.element.Element] | None = None
+    ):
+        super().__init__(
+            expected_type=marko.block.List,
+            children_parser=_ElementParser(
+                expected_type=marko.block.ListItem,
+                children_parser=children_parser,
+            ),
+        )
+
+
+class Paragraph[_V](_ElementParser[_V, marko.block.Paragraph]):
+    def __init__(
+        self, children_parser: base.IParser[_V, marko.element.Element] | None = None
+    ):
+        super().__init__(
+            expected_type=marko.block.Paragraph,
+            children_parser=children_parser,
+        )
+
+
+# TODO: rewrite parse children element on client side
+class Link[_V](_ElementParser[_V, marko.inline.Link]):
+    def __init__(
+        self,
+        text_expr: re.Pattern[str],
+        parse_element: Callable[[_V, marko.inline.Link], _V] | None = None,
+    ):
+        super().__init__(
+            expected_type=marko.inline.Link,
+            children_parser=base.Chain(RawText(text_expr), EOF()),
+            parse_element=parse_element,
+        )
--- dev-tools/main.py
+++ dev-tools/main.py
@@ -4,52 +4,56 @@ import argparse
 from dataclasses import dataclass
 from pathlib import Path
 import sys
-from typing import NoReturn
 from marko import Markdown
 import re
 
-from _validation import markdown as _md_validation
-from _validation import base as _base_validation
+import _parser.base
+import _parser.markdown
 
+import marko.element
 
-_change_item_validator = _base_validation.Chain(
-    _md_validation.ValidateParagraph(
-        _base_validation.Chain(
-            _md_validation.ValidateRawText(re.compile(r"^.+ \($")),
-            _md_validation.ValidateLink(re.compile(r"^#\d+$")),
-            _md_validation.ValidateRawText(re.compile(r"\)")),
+_change_item_parser: _parser.base.IParser[None, marko.element.Element] = (
+    _parser.base.Chain(
+        _parser.markdown.Paragraph(
+            _parser.base.Chain(
+                _parser.markdown.RawText(re.compile(r"^.+ \($")),
+                _parser.markdown.Link(re.compile(r"^#\d+$")),
+                _parser.markdown.RawText(re.compile(r"\)")),
+            ),
         ),
-    ),
-    _md_validation.ValidateEOF(),
+        _parser.markdown.EOF(),
+    )
 )
 
 
-_changelog_validator = _base_validation.Chain(
-    _md_validation.ValidateHeader(1, re.compile("Changes")),
-    _base_validation.Repeat(
-        _base_validation.Chain(
-            _md_validation.SkipBlankLines(),
-            _md_validation.ValidateHeader(2, re.compile(r"\[\d+\.\d+\]")),
-            _md_validation.SkipBlankLines(),
-            _md_validation.ValidateHeader(3, re.compile("External")),
-            _md_validation.SkipBlankLines(),
-            _md_validation.ValidateList(_change_item_validator),
-            _base_validation.Optional(
-                _base_validation.Chain(
-                    _md_validation.SkipBlankLines(),
-                    _md_validation.ValidateHeader(3, re.compile("Internal")),
-                ),
-                _base_validation.Chain(
-                    _md_validation.SkipBlankLines(),
-                    _md_validation.ValidateList(_change_item_validator),
+_changelog_parser: _parser.base.IParser[None, marko.element.Element] = (
+    _parser.base.Chain(
+        _parser.markdown.Header(1, re.compile("Changes")),
+        _parser.base.Repeat(
+            _parser.base.Chain(
+                _parser.markdown.BlankLines(),
+                _parser.markdown.Header(2, re.compile(r"\[\d+\.\d+\]")),
+                _parser.markdown.BlankLines(),
+                _parser.markdown.Header(3, re.compile("External")),
+                _parser.markdown.BlankLines(),
+                _parser.markdown.List(_change_item_parser),
+                _parser.base.Optional(
+                    _parser.base.Chain(
+                        _parser.markdown.BlankLines(),
+                        _parser.markdown.Header(3, re.compile("Internal")),
+                    ),
+                    _parser.base.Chain(
+                        _parser.markdown.BlankLines(),
+                        _parser.markdown.List(_change_item_parser),
+                    ),
                 ),
             ),
+            next_parser=_parser.base.Chain(
+                _parser.markdown.BlankLines(),
+                _parser.markdown.EOF(),
+            ),
         ),
-        next_validator=_base_validation.Chain(
-            _md_validation.SkipBlankLines(),
-            _md_validation.ValidateEOF(),
-        ),
-    ),
+    )
 )
 
 
@@ -66,16 +70,16 @@ def parse_args() -> Configuration:
     return Configuration(file=arguments.file)
 
 
-def main(conf: Configuration) -> NoReturn:
+def main(conf: Configuration) -> None:
     if not conf.file.exists():
         raise FileNotFoundError(f"File {conf.file} does not exist")
 
     md = Markdown()
     content = conf.file.read_text(encoding="utf-8")
     elements = md.parse(content).children
-    cursor = _md_validation.MarkdownCursor(elements)
+    cursor = _parser.markdown.MarkdownCursor(elements)
 
-    _changelog_validator.validate(cursor)
+    _changelog_parser.parse((None, cursor))
     print("Validation passed successfully!")
 
 
@@ -86,5 +90,5 @@ if __name__ == "__main__":
         main(configuration)
         exit(0)
     except Exception as e:
-        print(f"error: {e}", file=sys.stderr)
+        print(f"Error: {e}", file=sys.stderr)
         exit(1)
