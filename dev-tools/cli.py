ec3c305dc288fe31d97e922ad3d42b2ca11b53e5
Nifacy <alexgrishin5556@gmail.com>
2025-07-12 12:12:18 +0300

    add logger debug & public modes

--- dev-tools/cli.py
+++ dev-tools/cli.py
@@ -1,11 +1,33 @@
 import argparse
+from dataclasses import dataclass
+from datetime import datetime
 import logging
 from pathlib import Path
 import sys
+from typing import Final
 
 import _usecases
 
-type _ParsedArgs = _usecases.ValidateStructureArgs | _usecases.ValidateIssuesArgs | _usecases.ValidateIssueAddedArgs
+type _CommandArgs = _usecases.ValidateStructureArgs | _usecases.ValidateIssuesArgs | _usecases.ValidateIssueAddedArgs
+
+
+@dataclass
+class _ParsedArgs:
+    verbose: bool
+    command_args: _CommandArgs
+
+
+class _LoggerNameFormatter(logging.Formatter):
+    _DEFAULT_DATE_FORMAT: Final = "%Y-%m-%d %H:%M:%S"
+
+    def __init__(self, fmt: str | None = None, datefmt: str | None = None):
+        super().__init__(fmt=fmt, datefmt=datefmt)
+
+    def format(self, record: logging.LogRecord) -> str:
+        ct = datetime.fromtimestamp(record.created).strftime(
+            self.datefmt or self._DEFAULT_DATE_FORMAT
+        )
+        return f"{ct} [{record.levelname}] [{record.name}] {record.getMessage()}"
 
 
 def _init_validate_structure_parser(parser: argparse.ArgumentParser) -> None:
@@ -58,8 +80,16 @@ def _init_changelog_parser(parser: argparse.ArgumentParser) -> None:
 
 def _init_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="Dev Tools CLI")
-    subparsers = parser.add_subparsers(dest="command", required=True)
 
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        default=False,
+        help="increase output verbosity",
+    )
+
+    subparsers = parser.add_subparsers(dest="command", required=True)
     changelog_parser = subparsers.add_parser(
         "changelog", help="Changelog validation commands"
     )
@@ -68,7 +98,7 @@ def _init_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def _extract_parsed_arguments(args: argparse.Namespace) -> _ParsedArgs:
+def _extract_command_args(args: argparse.Namespace) -> _CommandArgs:
     match args.command:
         case "changelog":
             match args.changelog_command:
@@ -90,32 +120,72 @@ def _extract_parsed_arguments(args: argparse.Namespace) -> _ParsedArgs:
             raise ValueError(f"Unknown command: {args.command}")
 
 
+def _extract_parsed_arguments(args: argparse.Namespace) -> _ParsedArgs:
+    command_args = _extract_command_args(args)
+
+    return _ParsedArgs(
+        verbose=args.verbose,
+        command_args=command_args,
+    )
+
+
 def _parse_args() -> _ParsedArgs:
     parser = _init_parser()
     args = parser.parse_args()
     return _extract_parsed_arguments(args)
 
 
-def main() -> None:
-    args = _parse_args()
-    log = logging.getLogger("devtools-cli")
+def _init_public_logger(logger: logging.Logger) -> None:
+    info_handler = logging.StreamHandler(sys.stdout)
+    info_handler.setLevel(logging.INFO)
+    info_handler.addFilter(lambda record: record.levelno == logging.INFO)
+    info_handler.setFormatter(logging.Formatter("%(message)s"))
 
-    match args:
+    error_handler = logging.StreamHandler(sys.stderr)
+    error_handler.setLevel(logging.ERROR)
+    error_handler.addFilter(lambda record: record.levelno == logging.ERROR)
+    error_handler.setFormatter(logging.Formatter("%(message)s"))
+
+    logger.setLevel(logging.INFO)
+    logger.addHandler(info_handler)
+    logger.addHandler(error_handler)
+
+
+def _init_debug_logger(logger: logging.Logger) -> None:
+    debug_handler = logging.StreamHandler(sys.stderr)
+    debug_handler.setLevel(logging.DEBUG)
+    debug_handler.setFormatter(_LoggerNameFormatter())
+
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(debug_handler)
+
+
+def _init_logger(logger: logging.Logger, verbose: bool) -> None:
+    if verbose:
+        _init_debug_logger(logger)
+    else:
+        _init_public_logger(logger)
+
+
+def main(log: logging.Logger, args: _ParsedArgs) -> None:
+    match args.command_args:
         case _usecases.ValidateStructureArgs():
-            _usecases.validate_structure(args, log)
+            _usecases.validate_structure(args.command_args, log)
         case _usecases.ValidateIssuesArgs():
-            _usecases.validate_issues(args, log)
+            _usecases.validate_issues(args.command_args, log)
         case _usecases.ValidateIssueAddedArgs():
-            _usecases.validate_issue_added(args, log)
+            _usecases.validate_issue_added(args.command_args, log)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    args = _parse_args()
+    log = logging.getLogger("devtools-cli")
+    _init_logger(log, args.verbose)
 
     try:
-        main()
+        main(log, args)
     except Exception as e:
-        logging.error(f"Error: {e}")
+        log.error(f"Error: {e}")
         sys.exit(1)
     else:
         sys.exit(0)
