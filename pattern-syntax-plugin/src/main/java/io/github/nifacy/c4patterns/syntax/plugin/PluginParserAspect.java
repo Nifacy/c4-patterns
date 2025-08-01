4b6c7a07c232a7a7ce432822dc4c23b942fbcebb
Nifacy <alexgrishin5556@gmail.com>
2025-08-01 13:04:58 +0300

    migrate PatternSyntaxPatcher on AspectJ

---
+++ pattern-syntax-plugin/src/main/java/io/github/nifacy/c4patterns/syntax/plugin/PluginParserAspect.java
@@ -0,0 +1,39 @@
+package io.github.nifacy.c4patterns.syntax.plugin;
+
+import io.github.nifacy.c4patterns.syntax.parser.PatternParser;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import java.lang.reflect.Field;
+
+@Aspect
+public class PluginParserAspect {
+    @Around(
+        """
+                execution(
+                    * com.structurizr.dsl.PluginParser.parseParameter(
+                        com.structurizr.dsl.PluginDslContext,
+                        com.structurizr.dsl.Tokens
+                    )
+                ) && args(context, tokens)
+            """
+    )
+    public void aroundParseParameter(
+        ProceedingJoinPoint pjp,
+        Object context,
+        Object tokens
+    )
+        throws Throwable {
+        Field patternParserField = context.getClass().getDeclaredField("patternParser");
+        patternParserField.setAccessible(true);
+
+        PatternParser patternParser = (PatternParser) patternParserField.get(context);
+        if (patternParser != null) {
+            patternParser.parseBlockLine(PatchUtils.toTokenList(tokens));
+        } else {
+            pjp.proceed();
+        }
+    }
+
+}
