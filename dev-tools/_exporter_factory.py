90c2a862f1c97fe21c9196b7696e8e052947ad73
Nifacy <mai.grishin.alexey@gmail.com>
2026-01-02 20:23:10 +0300

    move exporter factory in separate module

---
+++ dev-tools/_exporter_factory.py
@@ -0,0 +1,75 @@
+from typing import Final, Protocol
+import zipfile
+
+import _exporters
+from _cached_downloader import CachedDownloader
+import _logging_tools
+import _release_extractor
+
+from pathlib import Path
+import logging
+
+
+_STRUCTURIZR_CLI_ARCHIVE_NAME: Final = "structurizr-cli.zip"
+_STRUCTURIZR_CLI_DIR: Final = "structurizr-cli"
+
+
+class ExporterFactory(Protocol):
+    def __call__(self, java_path: Path, syntax_plugin_path: Path) -> _exporters.StructurizrWorkspaceExporter:
+        ...
+
+
+def _get_structurizr_cli_exporter_factory(downloader: CachedDownloader, release: _release_extractor.StructurizrCliRelease, temp_dir_path: Path, log: logging.Logger) -> ExporterFactory:
+    structurizr_archive_path = temp_dir_path / _STRUCTURIZR_CLI_ARCHIVE_NAME
+    structurizr_cli_dir = temp_dir_path / _STRUCTURIZR_CLI_DIR
+
+    with _logging_tools.log_action(log, "Install structurizr cli"):
+        downloader.install_file(
+            url=release.url,
+            output_path=structurizr_archive_path,
+        )
+
+    with _logging_tools.log_action(log, "Extract structurizr cli"):
+        with zipfile.ZipFile(structurizr_archive_path) as archive:
+            archive.extractall(structurizr_cli_dir)
+
+    def _create_structurizr_cli_exporter(java_path: Path, syntax_plugin_path: Path) -> _exporters.StructurizrCli:
+        return _exporters.StructurizrCli(
+            structurizr_cli_dir=structurizr_cli_dir,
+            java_path=java_path,
+            syntax_plugin_path=syntax_plugin_path,
+        )
+
+    return _create_structurizr_cli_exporter
+
+
+def _get_structurizr_lite_exporter_factory(downloader: CachedDownloader, release: _release_extractor.StructurizrLiteRelease, temp_dir_path: Path, log: logging.Logger) -> ExporterFactory:
+    structurizr_lite_dir = temp_dir_path / "structurizr-lite"
+    structurizr_lite_dir.mkdir()
+
+    structurizr_lite_war_file = structurizr_lite_dir / "structurizr-lite.war"
+
+    with _logging_tools.log_action(log, "Install structurizr lite"):
+        downloader.install_file(
+            url=release.url,
+            output_path=structurizr_lite_war_file,
+        )
+
+    def _create_structurizr_lite_exporter(java_path: Path, syntax_plugin_path: Path) -> _exporters.StructurizrLite:
+        return _exporters.StructurizrLite(
+            structurizr_lite_dir=structurizr_lite_dir,
+            java_path=java_path,
+            syntax_plugin_path=syntax_plugin_path,
+            stdout_path=temp_dir_path / "stdout.txt",
+            stderr_path=temp_dir_path / "stderr.txt",
+        )
+
+    return _create_structurizr_lite_exporter
+
+
+def get_exporter_factory(downloader: CachedDownloader, release: _release_extractor.ExporterRelease, temp_dir_path: Path, log: logging.Logger) -> ExporterFactory:
+    match release:
+        case _release_extractor.StructurizrCliRelease() as cli_release:
+            return _get_structurizr_cli_exporter_factory(downloader, cli_release, temp_dir_path, log)
+        case _release_extractor.StructurizrLiteRelease() as lite_release:
+            return _get_structurizr_lite_exporter_factory(downloader, lite_release, temp_dir_path, log)
--- dev-tools/_usecases.py
+++ dev-tools/_usecases.py
@@ -4,14 +4,13 @@ import json
 import logging
 from pathlib import Path
 import tempfile
-from typing import Final, Iterator, Protocol
-import zipfile
+from typing import Final, Iterator
 
 import github
 import marko
 
 from _cached_downloader import CachedDownloader
-import _exporters
+import _exporter_factory
 import _parser.markdown
 
 import _change_log_parser
@@ -23,8 +22,6 @@ import _release_extractor
 
 
 _CUR_DIR_PATH: Final = Path(__file__).parent
-_STRUCTURIZR_CLI_ARCHIVE_NAME: Final = "structurizr-cli.zip"
-_STRUCTURIZR_CLI_DIR: Final = "structurizr-cli"
 _DOWNLOAD_CACHE_PATH: Final = _CUR_DIR_PATH / ".cache"
 
 
@@ -33,11 +30,6 @@ class _TestCaseInfo:
     name: str
     workspace_path: Path
     run_config: _integration_test_runner.TestCaseRunConfiguration
-
-
-class _ExporterFactory(Protocol):
-    def __call__(self, java_path: Path, syntax_plugin_path: Path) -> _exporters.StructurizrWorkspaceExporter:
-        ...
 
 
 @dataclass
@@ -291,62 +283,6 @@ def _extract_test_cases_info_from_file(config_file: Path) -> list[_TestCaseInfo]
     return test_cases_info
 
 
-def _get_structurizr_cli_exporter_factory(downloader: CachedDownloader, release: _release_extractor.StructurizrCliRelease, temp_dir_path: Path, log: logging.Logger) -> _ExporterFactory:
-    structurizr_archive_path = temp_dir_path / _STRUCTURIZR_CLI_ARCHIVE_NAME
-    structurizr_cli_dir = temp_dir_path / _STRUCTURIZR_CLI_DIR
-
-    with _logging_tools.log_action(log, "Install structurizr cli"):
-        downloader.install_file(
-            url=release.url,
-            output_path=structurizr_archive_path,
-        )
-
-    with _logging_tools.log_action(log, "Extract structurizr cli"):
-        with zipfile.ZipFile(structurizr_archive_path) as archive:
-            archive.extractall(structurizr_cli_dir)
-
-    def _create_structurizr_cli_exporter(java_path: Path, syntax_plugin_path: Path) -> _exporters.StructurizrCli:
-        return _exporters.StructurizrCli(
-            structurizr_cli_dir=structurizr_cli_dir,
-            java_path=java_path,
-            syntax_plugin_path=syntax_plugin_path,
-        )
-
-    return _create_structurizr_cli_exporter
-
-
-def _get_structurizr_lite_exporter_factory(downloader: CachedDownloader, release: _release_extractor.StructurizrLiteRelease, temp_dir_path: Path, log: logging.Logger) -> _ExporterFactory:
-    structurizr_lite_dir = temp_dir_path / "structurizr-lite"
-    structurizr_lite_dir.mkdir()
-
-    structurizr_lite_war_file = structurizr_lite_dir / "structurizr-lite.war"
-
-    with _logging_tools.log_action(log, "Install structurizr lite"):
-        downloader.install_file(
-            url=release.url,
-            output_path=structurizr_lite_war_file,
-        )
-
-    def _create_structurizr_lite_exporter(java_path: Path, syntax_plugin_path: Path) -> _exporters.StructurizrLite:
-        return _exporters.StructurizrLite(
-            structurizr_lite_dir=structurizr_lite_dir,
-            java_path=java_path,
-            syntax_plugin_path=syntax_plugin_path,
-            stdout_path=temp_dir_path / "stdout.txt",
-            stderr_path=temp_dir_path / "stderr.txt",
-        )
-
-    return _create_structurizr_lite_exporter
-
-
-def _get_exporter_factory(downloader: CachedDownloader, release: _release_extractor.ExporterRelease, temp_dir_path: Path, log: logging.Logger) -> _ExporterFactory:
-    match release:
-        case _release_extractor.StructurizrCliRelease() as cli_release:
-            return _get_structurizr_cli_exporter_factory(downloader, cli_release, temp_dir_path, log)
-        case _release_extractor.StructurizrLiteRelease() as lite_release:
-            return _get_structurizr_lite_exporter_factory(downloader, lite_release, temp_dir_path, log)
-
-
 def test_syntax_plugin(args: TestSyntaxPluginArgs, log: logging.Logger) -> None:
     exporter_releases = _release_extractor.extract_exporter_releases_from_file(args.env_config)
     downloader = CachedDownloader(log, _DOWNLOAD_CACHE_PATH)
@@ -366,7 +302,7 @@ def test_syntax_plugin(args: TestSyntaxPluginArgs, log: logging.Logger) -> None:
                     f"- Exporter: {exporter_release}\n"
                 )
 
-                exporter_factory = _get_exporter_factory(downloader, exporter_release, temp_dir_path, log)
+                exporter_factory = _exporter_factory.get_exporter_factory(downloader, exporter_release, temp_dir_path, log)
 
                 with _logging_tools.log_action(log, "Run integration tests"):
                     for test_case_info in test_cases_info:
